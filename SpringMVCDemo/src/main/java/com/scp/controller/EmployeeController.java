28fe7d51f1a3a44693bb732c6d722d096525765a
Sai <rajshripawase@gmail.com>
2018-04-26 00:14:30 +0530

    first

--- SpringMVCDemo/src/main/java/com/scp/controller/EmployeeController.java
+++ SpringMVCDemo/src/main/java/com/scp/controller/EmployeeController.java
@@ -2,14 +2,7 @@ package com.scp.controller;
 
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,12 +12,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.scp.dao.EmployeeDao;
 import com.scp.model.Employee;
-import com.scp.util.CustomErrorType;
-import com.scp.exception.*;
 
 @RestController
 @RequestMapping("/employee")
@@ -32,99 +22,31 @@ public class EmployeeController {
 
 	@Autowired
 	EmployeeDao empdao;
-	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
 	@GetMapping("/list")
 	public ResponseEntity<List<Employee>> getAllEmployee() {
-		List<Employee> emp = empdao.findAll();
-		if (emp.isEmpty()) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-			// You many decide to return HttpStatus.NOT_FOUND
-		}
-		return new ResponseEntity<List<Employee>>(emp, HttpStatus.OK);
+		return empdao.findAll();
+
 	}
 
 	@PostMapping("/empl")
-	public ResponseEntity<?> createEmployee(@RequestBody Employee emp, UriComponentsBuilder ucBuilder) {
-		// logger.info("Creating User : {}", emp);
+	public ResponseEntity<?> createEmployee(@RequestBody Employee emp) {
 
-		List<Employee> employeeList = empdao.findAll();
-		for (Employee employee : employeeList) {
-
-			if (emp.getEmpName().equals(employee.getEmpName())) {
-				// logger.error("Unable to create. A User with name {} already
-				// exist", emp.getEmpName());
-				return new ResponseEntity(
-						new ApiError(HttpStatus.CONFLICT, "Already exists",
-								new Throwable(
-										"Unable to create. A User with name " + emp.getEmpName() + " already exist")),
-						HttpStatus.CONFLICT);
-			}
-		}
-		empdao.save(emp);
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.setLocation(ucBuilder.path("/employee/empl/{id}").buildAndExpand(emp.getEmpId()).toUri());
-		return new ResponseEntity(new ApiError(HttpStatus.CREATED, "Record Created",
-				new Throwable(" A User with name " + emp.getEmpName() + " Created")), HttpStatus.CREATED);
-	}
-
-	@GetMapping("/list/{id}")
-	public ResponseEntity<?> getEmployeeById(@PathVariable("id") long empId) {
-
-		// logger.info("Fetching User with id {}", empId);
-		List<Employee> employeeList = empdao.findAll();
-		for (Employee employee : employeeList) {
-
-			if (empId == (employee.getEmpId())) {
-				// logger.error("User with id {} not found.", empId);
-				return new ResponseEntity<Employee>(employee, HttpStatus.OK);
-			}
-		}
-
-		return new ResponseEntity(new ApiError(HttpStatus.NOT_FOUND, "Record Not found",
-				new Throwable(" A User with name " + empId + " Not Found")), HttpStatus.NOT_FOUND);
+		return empdao.save(emp);
 
 	}
 
 	@PutMapping("/empl/{id}")
-	public ResponseEntity<?> updateEmployee(@PathVariable("id") long empId, @RequestBody Employee empdetails) {
-		// logger.info("Updating User with id {}", empid);
+	public ResponseEntity<?> updateEmployee(@PathVariable("id") long empId, @RequestBody Employee empdetails)
 
-		List<Employee> employeeList = empdao.findAll();
-		for (Employee employee : employeeList) {
-
-			if (empId == (employee.getEmpId())) {
-
-				employee.setEmpName(empdetails.getEmpName());
-				employee.setEmpAddress(empdetails.getEmpAddress());
-
-				empdao.save(employee);
-				return new ResponseEntity<Employee>(employee, HttpStatus.OK);
-			}
-		}
-		return new ResponseEntity(new ApiError(HttpStatus.NOT_FOUND, "Record Not Found",
-				new Throwable(" A User with name " + empId + " Not Found")), HttpStatus.NOT_FOUND);
+	{
+		return empdao.update(empId, empdetails);
 
 	}
 
 	@DeleteMapping("empl/{id}")
 	public ResponseEntity<?> deleteEmployee(@PathVariable("id") long empId) {
-		
-
-		List<Employee> employeeList = empdao.findAll();
-		for (Employee employee : employeeList) {
-
-			if (empId == (employee.getEmpId())) {
-				
-				empdao.delete(employee);
-				return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		}
-		return new ResponseEntity(
-				new ApiError(HttpStatus.NOT_FOUND, "Record Not Found",
-						new Throwable(" A User with name " + empId + " Not Found")),
-				HttpStatus.NOT_FOUND);
+		return empdao.delete(empId);
 
 	}
 
--- SpringMVCDemo/src/main/java/com/scp/dao/EmployeeDao.java
+++ SpringMVCDemo/src/main/java/com/scp/dao/EmployeeDao.java
@@ -1,57 +1,129 @@
 package com.scp.dao;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.scp.exception.ApiError;
 import com.scp.model.Employee;
 import com.scp.repository.EmployeeRepository;
 
 @Service
 public class EmployeeDao {
-	 
-	
+
 	@Autowired
 	EmployeeRepository emprespository;
-	
-	
-	 List<Employee> users=new ArrayList<>();
-	     
-	
-	
-	public Employee save(Employee emp){
-		return emprespository.save(emp);
-		
-	}
-	
-	public List<Employee> findAll()
-	{
-		return emprespository.findAll();
-	}
-	
-	public Employee findOne(long empid){
-		return emprespository.findOne(empid);	
+
+	List<Employee> users = null;
+
+	public ResponseEntity<ApiError> save(Employee emp) {
+
+		if (isUserExist(emp)) {
+			return new ResponseEntity<ApiError>(
+					new ApiError(HttpStatus.CONFLICT, "User Already Exists", new Throwable("user already Exists")),
+					HttpStatus.CONFLICT);
+		}
+
+		emprespository.save(emp);
+
+		return new ResponseEntity<ApiError>(new ApiError(HttpStatus.CREATED, "Record Created",
+				new Throwable(" A User with name " + emp.getEmpName() + " Created")), HttpStatus.CREATED);
+
 	}
 
-	public void delete(Employee employee){
-		emprespository.delete(employee);
+	public ResponseEntity<List<Employee>> findAll() {
+		List<Employee> empList = emprespository.findAll();
+
+		if (empList.isEmpty()) {
+
+			return new ResponseEntity(
+					new ApiError(HttpStatus.NO_CONTENT, "List is empty", 
+							new Throwable("list is empty")), HttpStatus.NO_CONTENT);
+			// You many decide to return HttpStatus.NOT_FOUND
+		}
+
+		return new ResponseEntity<List<Employee>>(empList, HttpStatus.OK);
 	}
+
+	public ResponseEntity<?> update(long empId, Employee empdetails) {
+
+		Employee employee = emprespository.findOne(empId);
+
+		if (emprespository.exists(empId)) {
+
+			employee.setEmpName(empdetails.getEmpName());
+			employee.setEmpAddress(empdetails.getEmpAddress());
+
+			emprespository.save(employee);
+			return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+		}
+
+		return new ResponseEntity(new ApiError(HttpStatus.NOT_FOUND, "Record Not Found",
+				new Throwable(" A User with name " + empId + " Not Found")), HttpStatus.NOT_FOUND);
+
+	}
+
+	public ResponseEntity<?> delete(long empId) {
+
+		Employee employee = emprespository.findOne(empId);
+
+		if (emprespository.exists(empId)) {
+
+			emprespository.delete(employee);
+			return new ResponseEntity(HttpStatus.OK);
+		}
+
+		return new ResponseEntity(new ApiError(HttpStatus.NOT_FOUND, "Record Not Found",
+				new Throwable(" A User with name " + empId + " Not Found")), HttpStatus.NOT_FOUND);
+	}
+
+	public Employee findOne(long empid) {
+		return emprespository.findOne(empid);
+	}
+
 	public boolean isUserExist(Employee emp) {
-	        return findByName(emp.getEmpName())!=null;
-	    }
+		return findByName(emp.getEmpName()) != null;
+	}
 
+	public Employee findByName(String name) {
+		users = emprespository.findAll();
 
-	 public Employee findByName(String name) {
-	        for(Employee emp : users){
-	            if(emp.getEmpName().equalsIgnoreCase(name)){
-	                return emp;
-	            }
-	        }
-	        return null;
-	    }
-	    
+		for (Employee emp : users) {
+			if (emp.getEmpName().equalsIgnoreCase(name)) {
+				return emp;
+			}
+		}
+		return null;
+	}
+
+	/*
+	 * @Autowired EmployeeRepository emprespository;
+	 * 
+	 * 
+	 * List<Employee> users=new ArrayList<>();
+	 * 
+	 * 
+	 * 
+	 * public Employee save(Employee emp){ return emprespository.save(emp);
+	 * 
+	 * }
+	 * 
+	 * public List<Employee> findAll() { return emprespository.findAll(); }
+	 * 
+	 * public Employee findOne(long empid){ return
+	 * emprespository.findOne(empid); }
+	 * 
+	 * public void delete(Employee employee){ emprespository.delete(employee); }
+	 * public boolean isUserExist(Employee emp) { return
+	 * findByName(emp.getEmpName())!=null; }
+	 * 
+	 * 
+	 * public Employee findByName(String name) { for(Employee emp : users){
+	 * if(emp.getEmpName().equalsIgnoreCase(name)){ return emp; } } return null;
+	 * }
+	 */
 
 }
